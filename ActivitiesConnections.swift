0058078438e82fad3317dbc7ed3390d1c106431e
Ryan Newhouse <newhouseboy2@gmail.com>
2016-11-07 17:14:17 -0700

    Added singleton for allowing easy access to important variables without passing them around painfully.

---
+++ ActivitiesConnections.swift
@@ -0,0 +1,9 @@
+//
+//  ActivitiesConnections.swift
+//  Proj Insight
+//
+//  Created by Ryan Newhouse on 2016-11-07.
+//  Copyright © 2016 Mackenzie Kary. All rights reserved.
+//
+
+import Foundation
